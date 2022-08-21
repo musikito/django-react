235c58bbfcb5407f3688097d1792b451f5acca18
JoséMiguel Marte <tumusicalatina@gmail.com>
2022-08-21 18:51:23 -0400

    created env

--- backend/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from myapp import views as myapp_views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", myapp_views.index),
-]
