47c187798fc8ca334ab556867fe70300530959bc
JoséMiguel Marte <tumusicalatina@gmail.com>
2022-09-07 18:01:14 -0400

    react front solo

--- backend/settings.py
+++ backend/settings.py
@@ -37,13 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework',  # Activamos el DRF
     'citas.apps.CitasConfig',  # Activamos la app citas
-    'frontend',  # Activamos la app frontend
+    'frontend.apps.FrontendConfig',  # Activamos la app frontend
+    'corsheaders',  # Activamos el corsheaders
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # corsheader
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# CORS HEADERS
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'backend.urls'
